d4baf9c3201d1392bbdd7351c958b0cf2ed42326
Rick Ogden <rick@rickogden.com>
2015-02-18 13:01:12 +0000

    The first iteration prototype of the scribble API.

--- src/app.php
+++ src/app.php
@@ -1,9 +1,64 @@
 <?php
 
+use \Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints as Assert;
+use \Symfony\Component\HttpFoundation\Response;
 /**
  * @var Silex\Application $app
  */
 $app = require_once __DIR__.'/bootstrap.php';
 
+$app['constraints.message'] = new Assert\Collection( [
+	'submitter' => new Assert\NotBlank(),
+	'email'     => new Assert\Email(),
+	'message'   => new Assert\NotBlank()
+] );
+
+$app->post( '/', function ( Request $request ) use ( $app ) {
+
+
+	/** @var \Symfony\Component\Validator\ValidatorInterface $validator */
+	$validator = $app['validator'];
+
+
+	// retrieve the data from the POST request
+	$data['submitter'] = $request->request->get( 'submitter', 'Anon' );
+	$data['email']     = $request->request->get( 'email' );
+	$data['message']   = $request->request->get( 'message' );
+
+	// validate user inputted data.
+	$errors = $validator->validate( $data, $app['constraints.message'] );
+	if ( count( $errors ) > 0 ) {
+		/** @var \Symfony\Component\Validator\ConstraintViolation $error */
+		$return = [ ];
+		foreach ( $errors as $error ) {
+			$r        = [ 'message' => $error->getMessage(), 'field' => $error->getPropertyPath() ];
+			$return[] = $r;
+		}
+
+		return $app->json( [ 'errors' => $return ], 400 );
+	}
+
+	/** @var MongoDB $mongodb */
+	$mongodb = $app['mongodb'];
+
+	$collection = $mongodb->selectCollection( 'messages' );
+
+	// Additional data to be entered into database
+	$data['ip']          = $request->getClientIp();
+	$data['submitDate']  = new MongoDate();
+	$data['messageType'] = 'website';
+
+	if ( $collection->insert( $data ) ) {
+
+		return new Response( null, 201 );
+
+	}
+
+
+	// This should never be reached unless something has gone wrong with the insert.
+	$app->abort( 500, "Something has gone wrong with this application." );
+
+} );
 
 return $app;
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,4 +4,21 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+require_once __DIR__ . '/config.php';
+
+/**
+ * Service to connect to MongoDB
+ * @var MongoDB;
+ */
+$app['mongodb'] = $app->share( function ( Silex\Application $app ) {
+	$client = new MongoClient( 'mongodb://' . $app['mongo.server.username'] . ':' . $app['mongo.server.password'] .
+	                           '@' . $app['mongo.server.hostname'] . ':' . $app['mongo.server.port'] . '/' . $app['mongo.server.db']
+	);
+	$db     = $client->selectDB( $app['mongo.server.db'] );
+
+	return $db;
+} );
+
+$app->register( new Silex\Provider\ValidatorServiceProvider() );
+
 return $app;
